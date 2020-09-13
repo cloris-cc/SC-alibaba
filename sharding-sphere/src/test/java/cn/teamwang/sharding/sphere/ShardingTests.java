27a1a89154c0191610493299e9b6434fdec7fbe7
jacky <clorisforcoding@gmail.com>
2020-09-13 20:49:12 +0800

    add sharding jdbc config

--- sharding-sphere/src/test/java/cn/teamwang/sharding/sphere/ShardingTests.java
+++ sharding-sphere/src/test/java/cn/teamwang/sharding/sphere/ShardingTests.java
@@ -2,6 +2,7 @@ package cn.teamwang.sharding.sphere;
 
 import cn.teamwang.sharding.sphere.entity.Order;
 import cn.teamwang.sharding.sphere.mapper.OrderMapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,12 +23,17 @@ public class ShardingTests {
     OrderMapper orderMapper;
 
     @Test
-    public void context() {
+    public void shardingDatabase() {
         // userId 10 % 2 = ds-0
         // user10 11 % 2 = ds-1
         int count = 0;
         count += orderMapper.insert(new Order().setUserId(10));
         count += orderMapper.insert(new Order().setUserId(11));
         assertThat(count).isEqualTo(2);
+    }
+
+    @Test
+    public void shardingDBandTable() {
+        System.out.println(orderMapper.selectOne(Wrappers.lambdaQuery(new Order().setUserId(2))));
     }
 }
