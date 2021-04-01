7f1d8285e8e594b7253314797fedd3cd6c98c629
jacky <jacky.teamwang@qq.com>
2021-04-02 05:58:23 +0800

    optimized aop log

--- rocketmq/rocketmq-spring-boot/src/main/java/cn/teamwang/rocketmq/boot/aop/SendMsgRemind.java
+++ rocketmq/rocketmq-spring-boot/src/main/java/cn/teamwang/rocketmq/boot/aop/SendMsgRemind.java
@@ -2,11 +2,11 @@ package cn.teamwang.rocketmq.boot.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import java.util.StringJoiner;
 
 /**
  * @author <a href="mailto:clorisforcoding@gmail.com">Jacky Fang</a>
@@ -27,6 +27,7 @@ public class SendMsgRemind {
         log.info("======> 消息发送完成");
     }
 */
+/*
     @Around(value = "execution(void cn.teamwang.rocketmq.boot.SendMessageTest.*(..))")
     public Object send(ProceedingJoinPoint point) {
         Object result = null;
@@ -41,21 +42,40 @@ public class SendMsgRemind {
         log.info("======> 消息发送完成(1/n)");
         return result;
     }
+*/
 
-    @Around(value = "execution(* cn.teamwang.rocketmq.boot.service.*.*(..))")
+    @Around(value = "execution(* cn.teamwang.rocketmq.boot.service.*.*(..)) " +
+            "&& !execution(* cn.teamwang.rocketmq.boot.service.TransactionListener.*(..))")
     public Object aroundHandle(ProceedingJoinPoint point) {
         // 获取方法参数
         Object args = point.getArgs();
+        String className = point.getTarget().getClass().getName();
+        String simpleName = className.substring(className.lastIndexOf(".") + 1);
         Object result = null;
-        log.info("======> 处理单条消息 {} 中...", args);
+        log.info("======> 类 {} 处理单条消息 {} 中...", simpleName, args);
 
         try {
             result = point.proceed();
+
+            log.info("======> 类 {} 单条消息 {} 处理完成", simpleName, args);
         } catch (Throwable throwable) {
-            log.error(throwable.getMessage());
+            /*
+            for (StackTraceElement element : throwable.getStackTrace()) {
+                log.error(element.toString());
+            }
+            */
+
+            // 同线程不安全的 StringBuilder
+            StringJoiner joiner = new StringJoiner("\n");
+            joiner.add(throwable.getMessage());
+            for (StackTraceElement element : throwable.getStackTrace()) {
+                joiner.add(element.toString());
+            }
+
+            log.error("======> 类 {} 单条消息 {} 处理失败", simpleName, args);
+            log.error(joiner.toString());
         }
 
-        log.info("======> 单条消息 {} 处理完成", args);
         return result;
     }
 }
